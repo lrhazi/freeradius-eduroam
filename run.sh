cc3afa2df66908e1a4b8bccd8703844018cf5b3b
Mohamed <lrhazi@gmail.com>
2015-03-12 23:28:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 sed -i -e "s/EDUROAM_SERVER_1/$EDUROAM_SERVER_1/g" -e "s/EDUROAM_SERVER_2/$EDUROAM_SERVER_2/g" /etc/freeradius/proxy.conf
 sed -i -e "s/EDUROAM_PASSWORD/$EDUROAM_PASSWORD/g" /etc/freeradius/proxy.conf
 
-freeradius -X
+freeradius -f
