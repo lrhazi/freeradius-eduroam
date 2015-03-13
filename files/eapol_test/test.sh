b361026758b22222364708714f3578522bc31b74
Mohamed <lrhazi@gmail.com>
2015-03-12 23:07:16 -0400

    Update test.sh

--- files/eapol_test/test.sh
+++ files/eapol_test/test.sh
@@ -3,9 +3,7 @@
 USERNAME=$1
 PASSWORD=$2
 
-server=$(grep freeradius /etc/hosts|awk '{print $1}')
-
 sed -e "s/USERNAME/$USERNAME/" -e "s/PASSWORD/$PASSWORD/" test.conf.template > test.conf
 
-eapol_test -a $server  -p1812 -stesting123 -c test.conf 
+eapol_test -a freeradius  -p1812 -stesting123 -c test.conf 
 
